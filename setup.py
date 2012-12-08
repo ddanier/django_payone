9ae0d80c2b6642619870d8e361d5f4038bd7f7cf
David Danier <david.danier@team23.de>
2012-12-08 23:20:51 +0100

    Added setup.py and cleanup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django_payone",
+    version = "0.1.0",
+    description = 'Payone client and server API',
+    author = 'David Danier',
+    author_email = 'david.danier@team23.de',
+    url = 'https://github.com/ddanier/django_payone',
+    long_description=open('README.rst', 'r').read(),
+    packages = [
+        'django_payone',
+        'django_payone.channel',
+    ],
+    package_data = {
+        'django_payone': ['static/payone/client.js'],
+    },
+    requires = [
+        'django(>=1.4)',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
+
